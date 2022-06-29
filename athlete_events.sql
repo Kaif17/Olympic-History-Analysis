52b8c2892d9e2601b8f2f7f2786c172f68e463ac
Kaif Choudhary <107095690+Kaif17@users.noreply.github.com>
2022-06-29 11:50:30 +0530

    Add files via upload

---
+++ athlete_events.sql
@@ -0,0 +1,111 @@
+use ec;
+select * from athlete_events;
+/*Q1)Show that players who is participated in Year 1992*/
+select * from athlete_events where year = '1992' ;
+
+
+/*Q2 show the games in earliest to latest with years*/
+select games from athlete_events 
+group by year order by games ;
+
+
+/*Q3 show city who taken part in Events*/
+select distinct city from athlete_events ;
+
+
+/*Q4 Find total number of summer olyampic games*/
+select  count(distinct games) from athlete_events 
+where season = "summer" ;
+
+
+
+/*Q5 Find  sports and games which is held in summer season */
+select distinct sport , games from athlete_events  
+where  season = "summer" 
+order by games ; 
+
+
+select * from athlete_events ;
+
+
+/*Q6 Find that players whose weight is 60 and height is 170 */
+select distinct name from athlete_events 
+where height = '170' and weight = '60' ;
+
+
+/*Q7 dont want that players whose taem is china */
+select name , team  from athlete_events 
+where not (team = "china" ) ;
+
+
+/*Q8 Find That players who won gold medal and  belongs team is italy*/
+select *  from athlete_events 
+where medal = "gold" and  team = "italy";
+
+
+/*Q9 top  athletes who has won the most gold medal */
+select name , count(medal) from athlete_events 
+where medal = "gold" 
+group by name 
+order by count(medal)desc  ;
+
+
+
+/*Q10 TOP 1 Athletes who Won most gold medal */
+select name, count(medal) from athlete_events 
+where medal ="gold" 
+group by name 
+order by count(medal) desc limit 1;
+
+
+/*Q11 list down total gold medal  won by each country */
+select country ,count(medal) 
+from athlete_events 
+where medal ="gold" 
+group by country 
+order by count(medal) desc;
+
+
+/*Q12 changing my colum name noc to Country*/
+alter table athlete_events change column noc Country varchar(50) ;
+
+
+
+/*Q13 list down total  silver medal won by each country */
+select country , count(medal) 
+from athlete_events 
+where medal ='silver' 
+group by country 
+order by count(medal) ;
+
+select * from athlete_events ;
+
+
+
+/*Q14 how to list down total gold, silver and bronze medals on by each country */
+select country, count(medal) as total_medal , medal 
+from athlete_events
+where medal <> "na" 
+group by country , medal ;
+
+
+/*Q15 How Many Sports has been held in olympic history */
+select count(distinct sport) 
+from athlete_events ;
+
+
+
+/*Q16 Which city has won Most Medal in olympic History */
+select city  ,count( medal) as total_medals 
+from athlete_events 
+where medal <> "na" 
+group by city 
+order by count(medal) desc ;
+
+
+
+/*Q17 Count the silver medal of city london */
+select city , count(medal) from athlete_events 
+where city = "london" and medal = "silver" 
+group by city 
+order by count(medal) ;
